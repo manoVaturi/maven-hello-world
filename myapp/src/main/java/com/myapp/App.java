7d0028069d7016d3a1550c46c227a09d7351045a
Or Mano Vaturi <88714317+manoVaturi@users.noreply.github.com>
2026-09-04 19:17:03 +0300

    feat: achanged app for testings

--- myapp/src/main/java/com/myapp/App.java
+++ myapp/src/main/java/com/myapp/App.java
@@ -3,7 +3,7 @@ package com.myapp;
 /**
  * Hello world!
  *
- *
+ *s
  *
  */
 public class App
@@ -11,5 +11,6 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello World! from Mano" );
+
     }
 }
