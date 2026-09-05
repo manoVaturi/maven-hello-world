873de569adf84198d955b086c50d272c9a18f943
Or Mano Vaturi <ormanor@gmail.com>
2026-09-05 21:29:22 +0300

    feat: lint all yaml files

--- myapp/src/main/java/com/myapp/App.java
+++ myapp/src/main/java/com/myapp/App.java
@@ -5,11 +5,9 @@ package com.myapp;
  *
  *
  */
-public class App
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World! from Mano" );
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World! from Mano");
 
     }
 }
